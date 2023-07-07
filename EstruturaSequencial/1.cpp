5ee59eefbb501a708cf39fe226509b1bf31df7d4
Amanda Moraes <amandamoraesmaria@gmail.com>
2023-07-07 01:19:07 -0300

    Delete EstruturaSequencial directory

--- EstruturaSequencial/1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int a,b,c,d;
-    cout << "Digite 4 números inteiros: ";
-    cin >> a >> b >> c >> d;
-    cout << "A soma dos quatro números são: "<<a + b + c + d;
-}
